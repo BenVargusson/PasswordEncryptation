836c499183f753d71a6c688b6e9ae4153b42f6ad
CityOfPuppets <CityOfPuppets@DESKTOP-644QAJI>
2021-12-13 19:51:21 -0300

    Agregar archivos de proyecto.

---
+++ PasswordEncryptation/DB/Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace PasswordEncryptation.DB
+{
+    class Data
+    {
+
+        private SqlConnectionStringBuilder cadenaConexion;
+        private SqlConnection sqlConnection;
+        private SqlCommand sqlCommand;
+        private SqlDataReader response;
+
+        public Data(string servidor, string bd, string user, string pass)
+        {
+            cadenaConexion = new SqlConnectionStringBuilder();
+            cadenaConexion.DataSource = servidor;
+            cadenaConexion.InitialCatalog = bd;
+            cadenaConexion.UserID = user;
+            cadenaConexion.Password = pass;
+
+
+        }
+
+    }
+}
---
+++ PasswordEncryptation/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using MD5Hash;
+
+namespace PasswordEncryptation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DB.Data d;
+        public MainWindow()
+        {
+            InitializeComponent();
+            d = new DB.Data("DESKTOP-644QAJI", "Evaluacion4", "sa", "casita123");
+        }
+
+        private void MD5Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string encrypt = TextToEncrypt.Text;
+            EncryptedText.Text = MD5Hash.Hash.Content(encrypt);
+
+            SqlConnectionStringBuilder scsb = new SqlConnectionStringBuilder();
+            scsb.DataSource = "DESKTOP-644QAJI";
+            scsb.InitialCatalog = "ProyectoAzure";
+            scsb.UserID = "sa";
+            scsb.Password = "casita123";
+
+            SqlConnection sqlConnection = new SqlConnection(scsb.ConnectionString);
+            sqlConnection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand("INSERT INTO TextoEncriptado VALUES (@Texto, @Encripcion)", sqlConnection);
+
+            sqlCommand.Parameters.Add("@Texto", SqlDbType.VarChar).Value = encrypt;
+            sqlCommand.Parameters.Add("@Encripcion", SqlDbType.VarChar).Value = EncryptedText.Text;
+
+            var response = sqlCommand.ExecuteNonQuery();
+
+            sqlConnection.Close();
+        }
+
+        private void SHA1Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SHA2Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AESButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            SqlConnectionStringBuilder scsb = new SqlConnectionStringBuilder();
+            scsb.DataSource = "DESKTOP-644QAJI";
+            scsb.InitialCatalog = "ProyectoAzure";
+            scsb.UserID = "sa";
+            scsb.Password = "casita123";
+
+            SqlConnection sqlConnection = new SqlConnection(scsb.ConnectionString);
+            sqlConnection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand("SELECT TEXTO FROM TEXTOENCRIPTADO WHERE ENCRIPCION = @Encripcion", sqlConnection);
+
+            sqlCommand.Parameters.Add("@Encripcion", SqlDbType.VarChar).Value = TextToEncrypt.Text;
+
+            var response = sqlCommand.ExecuteScalar();
+
+            EncryptedText.Text = Convert.ToString(sqlCommand.ExecuteScalar());
+            sqlConnection.Close();
+
+        }
+
+    }
+}
